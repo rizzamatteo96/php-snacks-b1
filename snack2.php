f52e29a65251b96084f80fcfd6ef8627de5882cb
rizzamatteo96 <77894068+rizzamatteo96@users.noreply.github.com>
2021-07-26 16:05:19 +0200

    finish project

--- snack2.php
+++ snack2.php
@@ -3,7 +3,18 @@
   // che name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero. 
   // Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato”
 
+  $name = $_GET['name'];
+  // var_dump($name);
+  $mail = $_GET['mail'];
+  // var_dump($mail);
+  $age = $_GET['age'];
+  // var_dump($age);
 
+  if(strlen($name) > 3 && (strpos($mail, '@') && strpos($mail, '.')) && is_numeric($age)){
+    echo 'Accesso riuscito';
+  } else{
+    echo 'accesso negato';
+  }
 
 ?>
 
